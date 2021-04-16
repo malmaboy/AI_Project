ed431a46994285e67cf6739ad301657fc65a6253
Hugo Carvalho <huguinho66@live.com>
2021-04-16 17:13:49 +0100

    TrouxaBot V2 added

--- TrouxaBot/TrouxaBot.cs
+++ TrouxaBot/TrouxaBot.cs
@@ -3,18 +3,21 @@ using System.Threading;
 using System.Collections.Generic;
 using ColorShapeLinks.Common;
 using ColorShapeLinks.Common.AI;
+using UnityEngine;
 
 namespace TrouxaBot
 {
     public class TrouxaBotThinker : AbstractThinker
     {
-       
+        // Current board state
+        private Winner winner;
+        
         // Maximum Negamax search depth
         private int maxDepth;
-        
+
         // Default maximum search depth
         private const int DEFAULT_MAXIMUM_DEPTH = 3;
-        
+
         /// <summary>
         /// Setup this AI 
         /// </summary>
@@ -23,19 +26,19 @@ namespace TrouxaBot
         {
             if (!int.TryParse(str, out maxDepth))
                 maxDepth = DEFAULT_MAXIMUM_DEPTH;
-            
+
             if (maxDepth < 1)
-               maxDepth = DEFAULT_MAXIMUM_DEPTH;
+                maxDepth = DEFAULT_MAXIMUM_DEPTH;
+
+            //losingMoves = new List<FutureMove>();
         }
 
 
         /// Returns the name of this AI which will include the maximum search depth
-        public override string ToString() => base.ToString() + "_V1";
+        public override string ToString() => "G03_" + base.ToString() + "_V2";
 
         public override FutureMove Think(Board board, CancellationToken ct)
         {
-            DateTime startTime = DateTime.Now;
-            
             // Invoke Negamax, starting with zero depth
             (FutureMove move, float score) decision = Negamax(board, ct,
                 board.Turn, board.Turn, 0, float.NegativeInfinity,
@@ -51,16 +54,14 @@ namespace TrouxaBot
             //Move to return 
             (FutureMove move, float score) selectedMove;
             
-            // Current board state
-            Winner winner;
-            
+
             // If a cancellation request was made 
             if (ct.IsCancellationRequested)
                 // set a "no move" and skip the remaining part of algorith
                 selectedMove = (FutureMove.NoMove, float.NaN);
-        
+
             // Otherwise
-            if ((winner = board.CheckWinner()) !=  Winner.None)
+            if ((winner = board.CheckWinner()) != Winner.None)
             {
                 if (winner.ToPColor() == player)
                     // AI player wins, return hightest possible score
@@ -71,17 +72,15 @@ namespace TrouxaBot
                 else
                     // A draw, return zero
                     selectedMove = (FutureMove.NoMove, 0f);
-                
+
             }
             // Apply Heuristic HERE!!!!!!
             else if (depth == maxDepth)
-                selectedMove = (FutureMove.NoMove, GetBoardScore(board, player));
+                selectedMove = (FutureMove.NoMove, Heuristics(board, player));
             else
             {
                 selectedMove = (FutureMove.NoMove, float.NegativeInfinity);
-                    
-                
-                
+
                 // Test each column
                 for (int i = 0; i < Cols; i++)
                 {
@@ -90,8 +89,7 @@ namespace TrouxaBot
                     for (int j = 0; j < 2; j++)
                     {
                         // Get current shape
-                        PShape shape = (PShape)j;
-                       
+                        PShape shape = (PShape) j;
 
                         // Use this variable to keep the current board's score
                         float eval;
@@ -102,55 +100,89 @@ namespace TrouxaBot
                         // Test move, call Negamax and undo move
                         board.DoMove(shape, i);
                         eval = -Negamax(
-                            board, ct, player.Other(), turn.Other(), depth + 1, 
-                            float.NegativeInfinity, 
+                            board, ct, player.Other(), turn.Other(), depth + 1,
+                            float.NegativeInfinity,
                             float.PositiveInfinity).score;
                         board.UndoMove();
-                        
+                            
+                            //Alpha beta cut
 
-                        //Alpha beta cut
-                        
                         if (eval > selectedMove.score)
                             // If Keep it
                             selectedMove = (new FutureMove(i, shape), eval);
-                        
+
                         // move so far?
                         if (eval > alpha)
                             // If so, keep it
                             alpha = eval;
-                        
+
                         if (beta < alpha)
                             return selectedMove;
 
+                        
                     }
                 }
             }
+
             return selectedMove;
         }
 
         /// Apply an Heuristic for winning
-        public static float GetBoardScore(Board board, PColor turn)
+        private  float Heuristics(Board board, PColor turn)
         {
-            return GetBoardScoreFor(board, turn) - GetBoardScoreFor(board,
-              turn.Other());
+            
+            // Play in the middle > GetBoardScoreFor
+            if (NeverPlayUnderWinningMove(board) >
+                GetBoardScoreFor(board, turn) - GetBoardScoreFor(board,
+                    turn.Other()))
+            {
+                // Return PlayInTheMiddle
+                return NeverPlayUnderWinningMove(board);
+            }
+            // GetBoardScoreFor > PlayInTheMiddle
+            if(GetBoardScoreFor(board, turn) - GetBoardScoreFor(board,
+                turn.Other()) > NeverPlayUnderWinningMove(board))
+            {
+                // return GetBoardScoreFor
+                return GetBoardScoreFor(board, turn) - GetBoardScoreFor(board,
+                    turn.Other());
+            }
+            
+            // GetBoardScoreFor > NeverPlayUnderWinningMove
+            if(GetBoardScoreFor(board, turn) - GetBoardScoreFor(board,
+                turn.Other()) > NeverPlayUnderWinningMove(board))
+            {
+                // Return GetBoardScoreFor
+                return GetBoardScoreFor(board, turn) - GetBoardScoreFor(board,
+                    turn.Other());
+            }
+            // NeverPlayUnderWinningMove > GetBoardScoreFor
+            if (NeverPlayUnderWinningMove(board) >
+                 GetBoardScoreFor(board, turn) - GetBoardScoreFor(board,
+                     turn.Other()))
+            {
+                // Return NeverPlayUnderWinningMove
+                return NeverPlayUnderWinningMove(board);
+            }
+            
+            return 0;
         }
-        
-        private static float GetBoardScoreFor(Board board, PColor turn)
+
+        private  float GetBoardScoreFor(Board board, PColor turn)
         {
             // Current heuristic value
             float score = 0;
             // To see if line is available
             bool lineAvailable = true;
-            
-            // Here ?
+
             for (int i = 0; i < board.rows; i++)
             {
-                
+
                 for (int j = 0; j < board.cols; j++)
                 {
                     // Get piece in current board position 
                     Piece? piece = board[i, j];
-                    
+
                     // Verify if there any piece
                     if (piece.HasValue)
                     {
@@ -158,12 +190,66 @@ namespace TrouxaBot
                         break;
                     }
                 }
-                if (lineAvailable) 
+
+                if (lineAvailable)
+                    
                     score++;
             }
-            
+
             // Return the final heuristic score
             return score;
         }
+
+        /// <summary>
+        /// Never play under opponent winning condition
+        /// </summary>
+        /// <param name="board"></param>
+        /// <returns>score</returns>
+        private float NeverPlayUnderWinningMove(Board board)
+        {
+            // Heuristic score
+            float score = 0;
+            // 
+            bool isWin    = false;
+            
+            for (int i = 0; i < board.rows; i++)
+            {
+                
+                if (board.IsColumnFull(i)) continue;
+
+                for (int j = 0; j < board.cols; j++)
+                {
+                    // Get current shape
+                    PShape shape = (PShape) j;
+                    
+                    // Verify if the position is valid
+                    if (j + 1 >= board.cols)
+                    {
+                        continue;
+                    }
+                    // Get Enemy board win position
+                    // +1 is above 
+                    Piece? enemyPiece = board[i, j + 1];
+                   
+                    // Verify if any piece in that position
+                    if (enemyPiece.HasValue)
+                    {
+                        // It is a win for the opponent
+                        isWin = true;
+                        // Less score
+                        score--;
+                        break;
+                    }
+                    // Need to be more precise
+                    if (isWin)
+                    { 
+                        score++;
+                    }
+                }
+            }
+
+            return score;
+        }
+        
     }
 }
